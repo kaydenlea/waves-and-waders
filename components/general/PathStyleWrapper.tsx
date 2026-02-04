3948a79ab79156cd063e465f1618003bb05ac0b2
roybse3113 <79131282+roybse3113@users.noreply.github.com>
2026-02-04 02:01:14 -0800

    fix stat table pager pill

--- components/general/PathStyleWrapper.tsx
+++ components/general/PathStyleWrapper.tsx
@@ -38,7 +38,7 @@ export default function PathStyleWrapper({
       <article
         id="content"
         className={cn(
-          "relative isolate overflow-hidden touch-pan-y w-full px-2 @min-4xl:pt-4 bg-background border-t border-x border-border/70 @min-4xl:border-none mx-auto scroll-mt-30",
+          "relative isolate overflow-clip touch-pan-y w-full px-2 @min-4xl:pt-4 bg-background border-t border-x border-border/70 @min-4xl:border-none mx-auto scroll-mt-30",
           // Keep drag overlays / floating edit controls above the footer.
           effectiveEditPage ? "z-auto" : "z-30",
           cls,
